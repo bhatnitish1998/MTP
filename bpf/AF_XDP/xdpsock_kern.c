a8dacb1e4ebb77370468931771b6a960d87ff4cc
bhatnitish1998 <bhatnitish1998@gmail.com>
2024-07-15 15:41:26 +0530

    changed directory structure

--- bpf/AF_XDP/xdpsock_kern.c
+++ bpf/AF_XDP/xdpsock_kern.c
@@ -14,7 +14,7 @@ struct {
 	__uint(value_size, sizeof(int));
 } xsks_map SEC(".maps");
 
-int num_socks = 0;
+int num_socks = 1;
 static unsigned int rr;
 
 SEC("xdp_sock") int xdp_sock_prog(struct xdp_md *ctx)
